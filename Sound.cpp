956229a70345cfb8678a3c41c6282920d0753e4b
dimitri <dimi309@users.noreply.github.com>
2014-12-15 01:22:26 +0100

    Added sound class.

---
+++ Sound.cpp
@@ -0,0 +1,22 @@
+/*
+ *  Sound.cpp
+ *
+ *  Created on: 2014-12-15
+ *      Author: Dimitri Kourkoulis
+ *              http://dimitros.be
+ *     License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#include "Sound.h"
+#include <miguel/vorbis/include/vorbis/codec.h>
+#include <miguel/vorbis/include/vorbis/vorbisfile.h>
+#include <dimitrikourk/portaudio/include/portaudio.h>
+
+namespace small3d {
+
+  Sound::Sound(){
+  }
+
+  Sound::~Sound(){
+  }
+}
---
+++ Sound.h
@@ -0,0 +1,37 @@
+/*
+ *  Sound.h
+ *
+ *  Created on: 2014-12-14
+ *      Author: Dimitri Kourkoulis
+ *              http://dimitros.be
+ *     License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#pragma once
+
+#include "Logger.h"
+
+namespace small3d {
+
+  /**
+   * @class Sound
+   * 
+   * @brief Sound management class
+   */ 
+
+  class Sound {
+
+  public:
+
+    /** 
+     * Constructor
+     */
+    Sound();
+    
+    /** 
+     * Destructor
+     */
+    ~Sound();
+  }
+
+}
