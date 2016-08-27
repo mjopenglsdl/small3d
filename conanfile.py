2f7a784e3969a3cf5870cab5f5897245f8371cfb
dimitri <dimi309@users.noreply.github.com>
2016-08-27 16:07:20 +0200

    Enabling sound for the conan.io package.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class Small3dConan(ConanFile):
     settings = "os", "arch", "build_type", "compiler"
     url="http://github.com/coding3d/small3d"
     requires = "SDL2/2.0.4@lasote/stable","SDL2_ttf/2.0.14@lasote/stable","glew/1.13.0@coding3d/ci", \
-        "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.7.6@dlarudgus20/stable"
+        "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.7.6@dlarudgus20/stable", \
+        "vorbis/1.3.5@coding3d/ci", "portaudio/19.20140130.0@coding3d/ci"
     license="https://github.com/coding3d/small3d/blob/master/LICENSE"
     exports = "CMakeLists.txt", "small3d/*", "samplegame/*","FindSMALL3D.cmake", "cmake/*", "glminclude/*"
 
