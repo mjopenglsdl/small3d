ec36864960faff9c01d5a1b2b78d190eb7bb2bdf
dimitri <dimi309@users.noreply.github.com>
2016-09-10 16:23:28 +0200

    Only left builds that work for now.

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=['x86_64'], args="--build glew freetype SDL2_ttf portaudio")
+    builder = ConanMultiPackager(archs=['x86_64'])
     builder.add_common_builds()
     builder.run()
