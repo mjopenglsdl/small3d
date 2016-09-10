8941676f650f6868300cdcd9a6f55b31acd9cdc7
dimitri <dimi309@users.noreply.github.com>
2016-09-10 11:09:46 +0200

    Setting up on travis ci.

---
+++ build.py
@@ -0,0 +1,7 @@
+from conan.packager import ConanMultiPackager
+import os
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(archs=['x86_64'])
+    builder.add_common_builds(pure_c=True)
+    builder.run()
