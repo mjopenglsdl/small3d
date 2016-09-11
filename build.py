771005f7e278c8fb5fd7f755aef7d8e3819717ea
dimitri <dimi309@users.noreply.github.com>
2016-09-11 23:32:12 +0200

    Explicit build declaration.

--- build.py
+++ build.py
@@ -1,7 +1,27 @@
 from conan.packager import ConanMultiPackager
-import os
+import os,platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=['x86_64'])
-    builder.add_common_builds()
+    builder = ConanMultiPackager(args="--build missing")
+    if platform.system() == 'Linux':
+        builder.add({'compiler.version': '4.8', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '4.9', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '5.2', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '5.3', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
+    
+        builder.add({'compiler.version': '4.8', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '4.9', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '5.2', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+        builder.add({'compiler.version': '5.3', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+    elif platform.system() == 'Darwin':
+        builder.add({'compiler.version': '7.3', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '7.0', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '6.1', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '6.0', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
+    
+        builder.add({'compiler.version': '7.3', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '7.0', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '6.1', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
+        builder.add({'compiler.version': '6.0', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
+    
     builder.run()
