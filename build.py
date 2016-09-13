4e4985bcfb8926d6bc0a4175a2dcc492c0913959
dimitri <dimi309@users.noreply.github.com>
2016-09-13 21:10:09 +0200

    Added Windows builds to build.py for appveyor.

--- build.py
+++ build.py
@@ -3,12 +3,15 @@ import os,platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
-    if platform.system() == 'Linux':
+    if platform.system() == 'Windows':
+        builder.add({'compiler.version': '14', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'Visual Studio', 'compiler.runtime': 'MT'})
+        builder.add({'compiler.version': '14', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'Visual Studio', 'compiler.runtime': 'MT'})
+    elif platform.system() == 'Linux':
         builder.add({'compiler.version': '4.8', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
         builder.add({'compiler.version': '4.9', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
         builder.add({'compiler.version': '5.2', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
         builder.add({'compiler.version': '5.3', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
-    
+
         builder.add({'compiler.version': '4.8', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
         builder.add({'compiler.version': '4.9', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
         builder.add({'compiler.version': '5.2', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
@@ -18,10 +21,10 @@ if __name__ == "__main__":
         builder.add({'compiler.version': '7.0', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
         builder.add({'compiler.version': '6.1', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
         builder.add({'compiler.version': '6.0', 'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'apple-clang'})
-    
+
         builder.add({'compiler.version': '7.3', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
         builder.add({'compiler.version': '7.0', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
         builder.add({'compiler.version': '6.1', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
         builder.add({'compiler.version': '6.0', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'apple-clang'})
-    
+
     builder.run()
