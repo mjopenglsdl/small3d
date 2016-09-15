46e82297393598775c612df760781bf36a1e4006
dimitri <dimi309@users.noreply.github.com>
2016-09-16 01:51:42 +0200

    Temporarily using coding3d sdl2_ttf

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class Small3dConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "build_type", "compiler"
     url="http://github.com/coding3d/small3d"
-    requires = "SDL2/2.0.4@lasote/stable","SDL2_ttf/2.0.14@lasote/stable","glew/1.13.0@coding3d/stable", \
+    requires = "SDL2/2.0.4@lasote/stable","SDL2_ttf/2.0.14@coding3d/ci","glew/1.13.0@coding3d/stable", \
         "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.7.6@dlarudgus20/stable", \
         "vorbis/1.3.5@coding3d/stable", "portaudio/master@jgsogo/stable"
     license="https://github.com/coding3d/small3d/blob/master/LICENSE"
