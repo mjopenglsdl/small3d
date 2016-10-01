d02e7158f8da55ee1d13a734bd49ad1b45f10e4f
dimitri <dimi309@users.noreply.github.com>
2016-10-01 22:07:36 +0200

    MD

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     filtered_builds = []
     for settings, options in builder.builds:
         if not (settings["compiler"] == "Visual Studio" and (settings["compiler.version"] != "14" or
-        settings["arch"] == "x86")) and \
+        settings["arch"] == "x86" or settings["compiler.runtime"] != "MD")) and \
         not ((settings["compiler"] == "gcc" and (settings["arch"] == "x86") or 
         settings["compiler.version"] == "4.6")):
             filtered_builds.append([settings, options])
