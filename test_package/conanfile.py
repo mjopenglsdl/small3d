5ecbaff4246031f32f13c0c8a1e744af7d9438eb
dimitri <dimi309@users.noreply.github.com>
2016-10-09 23:53:33 +0200

    Starting on version 1.0.7

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,7 +7,7 @@ username = os.getenv("CONAN_USERNAME", "coding3d")
 
 class TestGlew(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "small3d/1.0.6@%s/%s" % (username, channel)
+    requires = "small3d/1.0.7@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
