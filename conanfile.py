5858483323c72b504b654d892da4f9f920c6c651
dimitri <dimi309@users.noreply.github.com>
2016-11-01 09:24:46 +0100

    Starting version 1.1

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os, subprocess
 
 class Small3dConan(ConanFile):
     name = "small3d"
-    version = "1.0.11"
+    version = "1.1.0"
     ZIP_FOLDER_NAME = "%s-%s" % (name, version)
     generators = "cmake"
     settings = "os", "arch", "build_type", "compiler"
