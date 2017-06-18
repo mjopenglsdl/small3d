2e0827077514b94535df5e89159ab241e852e81f
dimitri <dimi309@users.noreply.github.com>
2017-06-18 18:46:08 +0200

    Temporarily removed unit tests

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class Small3dConan(ConanFile):
     url="http://github.com/dimi309/conan-packages"
     requires = "glfw/master@dimi309/stable", "freetype/2.6.3@lasote/stable","glew/2.0.0@dimi309/stable", \
         "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.8.4@dimi309/stable", \
-        "vorbis/1.3.5@dimi309/stable", "portaudio/rc.v190600.20161001@jgsogo/stable", "gtest/1.8.0@lasote/stable"
+        "vorbis/1.3.5@dimi309/stable", "portaudio/rc.v190600.20161001@jgsogo/stable"
     license="https://github.com/dimi309/small3d/blob/master/LICENSE"
     exports = ["small3d/*", "FindSMALL3D.cmake", "CMakeLists.txt"]
 
