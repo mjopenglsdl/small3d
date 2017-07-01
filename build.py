0766aef1ec7a193c41e51d9fc5e71c40d5138ba0
dimitri <dimi309@users.noreply.github.com>
2017-07-01 06:01:02 +0200

    Reintroducing build restrictions.

--- build.py
+++ build.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
     builder.add_common_builds()
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if not (settings["compiler"] == "Visual Studio" and (settings["compiler.runtime"] not in ("MD", "MDd"))) and \
-        not ((settings["compiler"] == "gcc" and (settings["arch"] == "x86") or settings["compiler.version"] == "4.6")):
+        if not (settings["compiler"] == "Visual Studio" and (settings["compiler.version"] != "14" or
+        settings["arch"] == "x86" or settings["compiler.runtime"] not in ("MD", "MDd"))) and \
+        not ((settings["compiler"] == "gcc" and (settings["arch"] == "x86") or 
+        settings["compiler.version"] == "4.6")):
             if settings["compiler"] == "gcc":
                 settings["compiler.libcxx"]="libstdc++11"
             filtered_builds.append([settings, options, env_vars, build_requires])
