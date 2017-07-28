98a9117cd715aecdb398c338e10cfa1032bf6240
dimitri <dimi309@users.noreply.github.com>
2017-07-28 04:37:01 +0200

    Documentation update

--- search/all_5.js
+++ search/all_5.js
@@ -3,7 +3,7 @@ var searchData=
   ['generatetexture',['generateTexture',['../classsmall3d_1_1_renderer.html#a42e2f3bc3d74c1efdf69609f81ec3b4c',1,'small3d::Renderer']]],
   ['getdata',['getData',['../classsmall3d_1_1_image.html#ab26bc22464edb7aefbcd4a2b95d788a2',1,'small3d::Image']]],
   ['getheight',['getHeight',['../classsmall3d_1_1_image.html#adf097899bb8212b43a6e620ff0d15007',1,'small3d::Image']]],
-  ['getinstance',['getInstance',['../classsmall3d_1_1_renderer.html#a1f1d5b1b46d4fa11f41278983b7e8a87',1,'small3d::Renderer']]],
+  ['getinstance',['getInstance',['../classsmall3d_1_1_renderer.html#a6fed933b938849667debfa58013e720b',1,'small3d::Renderer']]],
   ['getmodel',['getModel',['../classsmall3d_1_1_scene_object.html#a990a1b6f6f3ab08fd71f5c4a220afc4c',1,'small3d::SceneObject']]],
   ['getname',['getName',['../classsmall3d_1_1_scene_object.html#ae18cf681f49f719fa77f9b83bdb88da8',1,'small3d::SceneObject']]],
   ['getnumboxes',['getNumBoxes',['../classsmall3d_1_1_bounding_box_set.html#a0d8a4eac5d06e723ae60aef2bc2b0a83',1,'small3d::BoundingBoxSet']]],
