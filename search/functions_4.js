a8c2b158f3622485d019481cf6a12e4f88bb5a08
dimitri <dimi309@users.noreply.github.com>
2017-11-21 05:46:55 +0100

    Documentation update

--- search/functions_4.js
+++ search/functions_4.js
@@ -8,6 +8,7 @@ var searchData=
   ['getmodel',['getModel',['../classsmall3d_1_1_scene_object.html#a990a1b6f6f3ab08fd71f5c4a220afc4c',1,'small3d::SceneObject']]],
   ['getname',['getName',['../classsmall3d_1_1_scene_object.html#ae18cf681f49f719fa77f9b83bdb88da8',1,'small3d::SceneObject']]],
   ['getnumboxes',['getNumBoxes',['../classsmall3d_1_1_bounding_box_set.html#a0d8a4eac5d06e723ae60aef2bc2b0a83',1,'small3d::BoundingBoxSet']]],
+  ['gettokens',['getTokens',['../_get_tokens_8hpp.html#a86e7b190cee517441d9240c8c1898ea4',1,'small3d']]],
   ['getwidth',['getWidth',['../classsmall3d_1_1_image.html#a335588f99053b1f55c15c60c76c26624',1,'small3d::Image']]],
   ['getwindow',['getWindow',['../classsmall3d_1_1_renderer.html#a2065e4645034d538e5bbd40cd5fa3a50',1,'small3d::Renderer']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -2,26 +2,35 @@ var indexSectionsWithContent =
 {
   0: "abcdfgilmnoprstuvw~",
   1: "bilmrs",
-  2: "abcdgilmoprsw~",
-  3: "bcfilnoprtuv",
-  4: "cms"
+  2: "bgilmrs",
+  3: "abcdgilmoprsw~",
+  4: "bcfilnoprtuv",
+  5: "l",
+  6: "l",
+  7: "cms"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions",
-  3: "variables",
-  4: "pages"
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "defines",
+  7: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions",
-  3: "Variables",
-  4: "Pages"
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Macros",
+  7: "Pages"
 };
 
